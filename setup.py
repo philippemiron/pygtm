5492eafd460188a304c3a5a0e355b14c1df1c5d2
Philippe Miron, Ph. D <philippemiron@gmail.com>
2022-06-03 15:12:27 -0400

    black fix setup syntax

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 from setuptools import setup
 
-setup(name='pygtm',
-      version='0.1',
-      description='python Geospatial Transition Matrix (pygtm) toolsets',
-      url='http://github.com/philippemiron/pygtm',
-      author='Philippe Miron',
-      author_email='pmiron@fsu.edu',
-      license='MIT',
-      packages=['pygtm'],
-      zip_safe=False,
-      install_requires=['numpy', 'scipy', 'cartopy', 'scikit-learn', 'matplotlib', 'netCDF4', 'cmocean']
-      )
+setup(
+    name="pygtm",
+    version="0.1",
+    description="python Geospatial Transition Matrix (pygtm) toolsets",
+    url="http://github.com/philippemiron/pygtm",
+    author="Philippe Miron",
+    author_email="pmiron@fsu.edu",
+    license="MIT",
+    packages=["pygtm"],
+    zip_safe=False,
+    install_requires=[
+        "numpy",
+        "scipy",
+        "cartopy",
+        "scikit-learn",
+        "matplotlib",
+        "netCDF4",
+        "cmocean",
+    ],
+)
